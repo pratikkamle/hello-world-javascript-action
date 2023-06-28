67eec75d1063f94a21a5a7938fa37649b8148e45
Pratik Kamle <pratik.kamle17@gmail.com>
2023-06-28 14:04:43 +0530

    Update index.js with axios http client
    Updated the script with a function to do the HTTP method operations.

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
+
+async function run() {
+  try {
+    // Get inputs using the GitHub Core library
+    const data = core.getInput('data');
+    const url = core.getInput('url');
+    const headers = core.getInput('customHeaders');
+    const method = core.getInput('method');
+
+    // Parse the headers input as JSON
+    const parsedHeaders = JSON.parse(headers);
+
+    // Create the request body
+    const requestBody = {
+      data: data
+    };
+
+    // Set up the axios request configuration
+    const axiosConfig = {
+      method: method.toUpperCase(),
+      url: url,
+      headers: parsedHeaders,
+      data: requestBody
+    };
+
+    // Send the HTTP request
+    const response = await axios(axiosConfig);
+
+    // Log the response
+    console.log('Response:', response.data);
+  } catch (error) {
+    // Handle any errors that occur during the execution
+    console.error('Error:', error.message);
+    process.exit(1);
+  }
+}
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,6 +47,8 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  // calling the function
+  run();
 } catch (error) {
   core.setFailed(error.message);
 }
